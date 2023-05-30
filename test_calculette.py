b8d3fb6331a04fac69683e4c4ee22a9bf90e07a2
0pahh <Thierry.Angel@protonmail.com>
2023-05-30 11:13:18 +0200

    passing test to check actions trigger

--- test_calculette.py
+++ test_calculette.py
@@ -7,12 +7,12 @@ def cal():
 
 
 def test_add(cal):
-	cal.add(1,665)
+	cal.add(1,666)
 	assert cal.res == 667
 
 def test_div(cal):
-	cal.divide(1,8)
-	assert cal.res == 0.85
+	cal.divide(1,4)
+	assert cal.res == 0.25
 
 def test_raise(cal):
 	with pytest.raises(Error):
