cbdc0d27dad99e253f9a0c233ea231948ac32449
0pahh <Thierry.Angel@protonmail.com>
2023-05-30 11:10:15 +0200

    init repository

---
+++ test_calculette.py
@@ -0,0 +1,19 @@
+import pytest
+from calculette import Calculette, Error
+
+@pytest.fixture
+def cal():
+    return Calculette()
+
+
+def test_add(cal):
+	cal.add(1,665)
+	assert cal.res == 667
+
+def test_div(cal):
+	cal.divide(1,9)
+	assert cal.res == 0.85
+
+def test_raise(cal):
+	with pytest.raises(Error):
+		cal.divide(1,0)
